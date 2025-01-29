07ef5af631c5b39f81b66728c1ae9df59ee35aea
hhartlen <heatherhartlen9@gmail.com>
2025-01-29 16:29:53 -0400

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,17 +1,18 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-#define BUFSIZE 256
-    
-// This program prints the size of a specified file in bytes
+
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    // Use a safer approach with execvp instead of system
+    char *args[] = {"wc", "-c", argv[1], NULL};
+    if (execvp("wc", args) == -1) {
+        perror("Error executing wc");
+        return -1;
+    }
+
+    return 0;
 }
